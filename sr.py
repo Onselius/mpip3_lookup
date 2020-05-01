12ed80b2a8c15160fcd46b99864f3db4b5f98d91
Onselius <lars.onselius@gmail.com>
2020-05-01 23:22:48 +0200

    Restructured code

--- sr.py
+++ sr.py
@@ -1,7 +1,6 @@
 #! /bin/python3
 
 import requests
-import json
 import datetime
 import re
 import sqlite3
@@ -9,6 +8,41 @@ import sqlite3
 def main():
     db = sqlite3.connect("episodes.db")
     cursor = db.cursor()
+
+    if update_db(cursor):
+        db.commit()
+    
+    search_episode(cursor)
+    
+    db.close()
+
+
+def update_db(cursor):
+    latest_episode = check_latest_episode_in_db(cursor)
+    url = "http://api.sr.se/api/v2/episodes/index?programid=2024&fromdate=2009-01-01&todate=2009-02-10&page=1&size=25&format=json"
+    #url = "http://api.sr.se/api/v2/episodes/index?programid=2024&fromdate=" + str(latest_episode) + "&page=1&size=25&format=json"
+
+    number_of_episodes = 0
+
+    while True:
+        break
+        response = requests.get(url)
+        json_response = response.json()
+        insert_episodes_into_db(cursor, json_response["episodes"])
+        number_of_episodes += int(json_response["pagination"]["size"])
+        print(f"Uppdaterat databasen med {number_of_episodes} avsnitt totalt")
+        try:
+            url = json_response["pagination"]["nextpage"]
+        except KeyError as identifier:
+            print("No more pages")
+            break
+    if number_of_episodes:
+        return True
+    return False
+
+
+
+def check_latest_episode_in_db(cursor):
     cursor.execute("SELECT published FROM episodes ORDER BY published DESC")
     try:
         latest_episode = convert_utc_to_date(cursor.fetchone()[0])
@@ -16,39 +50,21 @@ def main():
     except TypeError as identifier:
         latest_episode = "2009-01-01"
         print(f"Inget avsnitt inlagt, använder 2009-01-04 för att fylla databasen")
+    return latest_episode
 
-    url = "http://api.sr.se/api/v2/episodes/index?programid=2024&fromdate=2009-01-01&todate=2009-02-10&page=1&size=10&format=json"
-    #url = "http://api.sr.se/api/v2/episodes/index?programid=2024&fromdate=" + str(latest_episode) + "&page=1&size=10&format=json"
-
-    number_of_episodes = 0
-
-    while url:
-        break
-        response = requests.get(url)
-        output = response.json()
-        update_db(cursor, output["episodes"])
-        number_of_episodes += int(output["pagination"]["size"])
-        print(f"Uppdaterat databasen med {number_of_episodes} avsnitt totalt")
-        try:
-            url = output["pagination"]["nextpage"]
-        except KeyError as identifier:
-            print("No more pages")
-            break
-    
-    search_episode(cursor)
-    
-    db.commit()
-    db.close()
 
 def search_episode(cursor):
     search_term = input("Ange sökterm: ")
     print()
     terms = search_term.split()
-    
-    cursor.execute(f"SELECT * FROM episodes WHERE description LIKE '%{search_term}%'")
+    sql_string = f"SELECT * FROM episodes WHERE description LIKE '%{terms[0]}%'"
+    for i in range(1, len(terms)):
+        sql_string += f" AND description LIKE '%{terms[i]}%'"
+    cursor.execute(sql_string)
     for row in cursor:
         print_episode(row)
         print()
+    print()
 
 
 def print_episode(episode_info):
@@ -70,7 +86,7 @@ def convert_utc_to_date(utcdate):
     return human_date.date()
 
 
-def update_db(cursor, episodes):
+def insert_episodes_into_db(cursor, episodes):
     for episode in episodes:
         date = extract_date_from_string(episode['publishdateutc'])
         #print("Id: " + str(episode["id"]))
@@ -84,5 +100,5 @@ def update_db(cursor, episodes):
         except sqlite3.IntegrityError as identifier:
             pass
 
-
-main()
+if __name__ == "__main__":
+    main()
